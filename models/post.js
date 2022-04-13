4a9c074b63db3d66a9c34fb91d9a30e9e9a747fd
kshma29 <kshmasingh8475@gmail.com>
2022-04-13 10:32:32 +0530

    Supplies from Amber

---
+++ models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types // Object Id has to be either deconstructed earlier or written like mongoose.Schema.Types.ObjectId
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    photo:{
+        type:String,
+        required:true
+    },
+    likes:[{type:ObjectId,ref:"User"}],
+    comments:[{
+        text:String,
+        postedBy:{type:ObjectId,ref:"User"}
+    }],
+    postedBy:{
+       type:ObjectId,
+       ref:"User"
+    }
+},{timestamps:true})
+
+mongoose.model("Post",postSchema)
